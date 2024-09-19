08a442a9686495d6f5d1eca72b6d8c5a305dd2cc
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-09-19 19:27:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+const Calc = (digit, digit2, sign) => {
+    if (sign == "-") {
+        console.log(digit - digit2);
+    }
+};
+Calc(25, 35, "-");
+function alisession() {
+    let ali = "ali";
+    console.log(ali);
+}
+alisession();
---
+++ app.ts
@@ -0,0 +1,13 @@
+const Calc =(digit: number,digit2:number, sign:string) => {
+        if(sign=="-"){
+        console.log(digit - digit2)
+    } }
+   
+   Calc(25,35,"-")
+   function alisession(){
+    let ali ="ali"
+    console.log(ali)
+
+   }
+   alisession()
+   
